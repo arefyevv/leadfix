707b0367284aec01630058f3f48f325cd093b813
Viktor <viktor082rus@gmail.com>
2026-06-02 21:59:10 +0300

    update

--- src/components/SiteFooter.tsx
+++ src/components/SiteFooter.tsx
@@ -24,9 +24,20 @@ export function SiteFooter() {
         </div>
 
         <div className="site-footer__bottom">
-          <span>© 2026 LeadFix</span>
-          <span>НПД Арефьев В.О.</span>
-          <span>ИНН 910907646492</span>
+          <div className="site-footer__details">
+            <span>© 2026 LeadFix</span>
+            <span>НПД</span>
+            <span>ИНН 910907646492</span>
+          </div>
+          <div className="site-footer__contacts">
+            <a href="https://t.me/leadfix_support" target="_blank" rel="noreferrer" aria-label="Telegram LeadFix">
+              <svg viewBox="0 0 24 24" aria-hidden="true">
+                <path d="M20.7 4.1 3.9 10.6c-1.1.4-1.1 1.1-.2 1.4l4.3 1.3 1.7 5.1c.2.7.1.9.8.9.5 0 .8-.2 1-.4l2.1-2 4.4 3.2c.8.5 1.4.2 1.6-.8l2.8-13.4c.3-1.2-.5-1.8-1.7-1.3ZM9.1 13l8.4-5.3c.4-.2.8-.1.5.2l-6.9 6.3-.3 3.1-1.7-4.3Z" />
+              </svg>
+              <span>Telegram</span>
+            </a>
+            <a href="mailto:hello@leadfix.ru">hello@leadfix.ru</a>
+          </div>
         </div>
       </div>
     </footer>
